5bb3f5c6954b67b0336847d9aa0d503b7e5e2dec
adugyan <44382721+adugyan@users.noreply.github.com>
2019-10-29 14:56:44 -0500

    Create InverseKinematics.java

---
+++ InverseKinematics.java
@@ -0,0 +1,31 @@
+//Practice file to get segemnts moving and get used to using IK
+class Segment {
+    Pvector a;
+    float angle;
+    float len;
+    
+    Pvector b = new Pvector();
+    
+    //Constructor
+    Segment(float x, float y, float len_, float angle_){
+        a = new Pvector(x,y);
+        len = len_;
+        angle = angle_;
+    }
+    void calculateB() {
+        float dx = len * cos(angle);
+        float dy = len * sin(angle);
+        b.set(a.x+dx, a.y+dy);
+        
+    }
+    
+    void update() {
+     calculateB();   
+    }
+    
+    void show(){
+        stroke(255);
+        strokeWeight(4);
+        line(a.x, a.y, b.x, b.y)
+    }
+}
